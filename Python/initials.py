35ea031463d7fa5fe983493ccfbeab2e65342e02
swagoverlord <azcooldude8@gmail.com>
2022-05-11 18:07:39 +0200

    Initials

---
+++ Python/initials.py
@@ -0,0 +1,11 @@
+#First name: Alex
+#Last name: *****
+#Fun fact: The Sun will devour the Earth
+#          in approximately 7 billion years
+
+print("  AAA  ")
+print(" A   A ")
+print("A     A")
+print("AAAAAAA")
+print("A     A")
+print("A     A")
